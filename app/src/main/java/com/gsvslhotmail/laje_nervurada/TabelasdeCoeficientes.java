097558ce1aafb0d943429ace9a000b36891bd435
Davi Alves Magalhães <davi.magalhaes@ifpb.edu.br>
2017-03-09 22:09:48 -0300

    Criado método que recebe o lambda e devolve os coeficientes correspondentes.

--- app/src/main/java/com/gsvslhotmail/laje_nervurada/TabelasdeCoeficientes.java
+++ app/src/main/java/com/gsvslhotmail/laje_nervurada/TabelasdeCoeficientes.java
@@ -1,26 +1,35 @@
 package com.gsvslhotmail.laje_nervurada;
 
 
-public class TabelasdeCoeficientes {
+class TabelasdeCoeficientes {
 
-    public double arredondar(double lambda) {
-        String s = "" + lambda;
+    private static double [][] coeficientes =
+            {{1.0,  4.23,   4.23,   2.5,    2.5},
+            {1.05,  4.62,   4.25,   2.62,   2.5},
+            {1.1,   5.0,    4.27,   2.73,   2.5},
+            {1.15,  5.38,   4.25,   2.83,   2.5},
+            {1.2,   5.75,   4.22,   2.92,   2.5},
+            {1.25,  6.1,    4.17,   3.0,    2.5},
+            {1.3,   6.44,   6.12,   3.08,   2.5},
+            {1.35,  6.77,   4.06,   3.15,   2.5},
+            {1.4,   7.1,    4.0,    3.21,   2.5},
+            {1.45,  7.41,   3.95,   3.28,   2.5},
+            {1.5,   7.72,   3.89,   3.33,   2.5},
+            {1.55,  7.99,   2.82,   3.39,   2.5}};
 
-        String split[];
-        split = s.split("");
-
-        if (split.length == 3)
-            return lambda;
-        Double resultado = Double.parseDouble(split[0]);
-        Double aux1 = Double.parseDouble(split[2]);
-        Double aux2 = Double.parseDouble(split[3]);
-        if (aux2 > 5) {
-            aux1++;
-            aux2 = 0.0;
-        } else if (aux2 < 5)
-            aux2 = 5.0;
-        resultado = resultado + (aux1 / 10) + (aux2 / 100);
-        return resultado;
+    /**
+     * Recebe um valor lambda e retorna a linha contendo os coeficientes correspondentes
+     * @param lambda valor lambda
+     * @return vetor de doubles contendo os coeficientes
+     */
+    static double[] getCoeficientes(double lambda){
+        System.out.println("Tamanho: " + coeficientes.length);
+        for(int i = 0; i < coeficientes.length; i++){
+            if(coeficientes[i][0] > lambda){
+                return coeficientes[i];
+            }
+        }
+        return null;
     }
 }
 
